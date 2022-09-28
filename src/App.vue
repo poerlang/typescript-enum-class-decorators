473467a3f51ba8f44f1cdde0961132b18986589d
poerlang <30558209@qq.com>
2022-09-28 17:20:49 +0800

    1

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<script setup lang="ts">
+import {testEnums} from "./MyEnum";
+import {Ref, ref} from "vue";
+
+const time = ref('...')
+const ret: Ref<Array<any>> = ref(['click the button'])
+const onClick = () => {
+  ret.value.length = 0;
+  time.value = new Date().getTime()+'';
+  testEnums().forEach(v => {
+    ret.value.push(v);
+  });
+}
+</script>
+
+<template>
+  <div>{{ time }}</div>
+  <div>{{ ret }}</div>
+  <button class="btn" @click="onClick">tick</button>
+</template>
+
+<style scoped>
+.btn {
+  background: #185e56;
+  padding: 5px;
+  color: #f3f4ff;
+  transition: all 0.3s;
+}
+.btn:hover{
+  background: #248c80;
+}
+.btn:active{
+  background: #124640;
+}
+</style>
---
+++ src/MyEnum.ts
@@ -0,0 +1,49 @@
+export class Enum extends Number {
+    private static ___value_to_key___: any = {}
+    private static ___value_to_info___: any = {}
+
+    static valueToKey(value: any) {
+        return this.___value_to_key___[value]
+    }
+
+    static valueToInfo(value: any) {
+        return this.___value_to_info___[value]
+    }
+}
+
+export function AddInfo(info: any) {
+    return (a_enum_class: any | Enum, key: any) => {
+        const value = a_enum_class[key];
+        a_enum_class.___value_to_key___[value] = key;
+        a_enum_class.___value_to_info___[value] = info;
+    }
+}
+
+export class FooEnum extends Enum {
+    @AddInfo('一') static one = 1;
+    @AddInfo('二') static two = 2;
+    @AddInfo('三') static three = 3;
+}
+
+export const testEnums = () => {
+    let theType: FooEnum;
+    let value = Math.random();
+    if(value>0.3333){
+        if(value>0.6666){
+            theType = FooEnum.three
+        }else{
+            theType = FooEnum.two
+        }
+    }else{
+        theType = FooEnum.one;
+    }
+    let output;
+    if (theType === FooEnum.one) {
+        output = ['the enum = 1 ---->', FooEnum.valueToKey(FooEnum.one), FooEnum.valueToInfo(FooEnum.one), FooEnum.one]
+    } else if (theType === FooEnum.two){
+        output = ['the enum = 2 ---->',  FooEnum.valueToKey(FooEnum.two), FooEnum.valueToInfo(FooEnum.two), FooEnum.two]
+    }else{
+        output = ['the enum = 3 ---->',  FooEnum.valueToKey(FooEnum.three), FooEnum.valueToInfo(FooEnum.three), FooEnum.three];
+    }
+    return output;
+}
